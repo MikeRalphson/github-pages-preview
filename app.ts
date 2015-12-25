9982f801fb2ec8e68de81e08d83756962d182639
Damian Connolly <Damian Connolly>
2015-12-25 21:38:36 +0100

    Added the Content class to help parse our site content into

--- app.ts
+++ app.ts
@@ -5,18 +5,25 @@ import Path			= require( 'path' );
 import RMRF			= require( 'rimraf' );
 import YAML			= require( 'js-yaml' );
 import ConfigHelper	= require( './src/ConfigHelper' );
+import Content		= require( './src/Content' );
 import YAMLConfig	= require( './src/YAMLConfig' );
 
 // declare our vars
 var log:Bunyan.Logger 		= null;
 var config:ConfigHelper		= null;
 var yamlConfig:YAMLConfig	= null;
+var posts:Content[]			= [];
+var pages:Content[]			= [];
 
 function start():void
 {
 	_createLog();
 	_readToolConfig();
 	_readYAMLConfig();
+	
+	_readContents( "_posts/", posts );
+	_readContents( "pages/", pages );
+	log.info( posts.length + " posts and " + pages.length + " pages were read" );
 }
 start();
 
@@ -56,4 +63,30 @@ function _readYAMLConfig():void
 	yamlConfig = new YAMLConfig();
 	yamlConfig.fromObj( yaml );
 	log.debug( "YAML config:", yamlConfig );
+}
+
+// reads the contents of a folder
+function _readContents( dir:string, ar:Content[] ):void
+{
+	var path = Path.join( config.src.path + dir );
+	FS.readdirSync( path ).forEach( function( filename ){
+		
+		// make sure it's a html file
+		if( filename.lastIndexOf( ".html" ) !== ( filename.length - 5 ) )
+		{
+			log.info( "Ignoring the file '" + filename + "' as it's not a HTML file" );
+			return;
+		}
+		
+		// create our content object
+		var contentsRaw:string 	= FS.readFileSync( Path.join( path, filename ), "utf-8" );
+		var content:Content 	= new Content();
+		content.readFromFile( filename, contentsRaw );
+		
+		ar.push( content );
+	});
+	
+	ar.sort( function( a:Content, b:Content ){
+		return a.date - b.date;
+	});
 }
---
+++ src/Content.ts
@@ -0,0 +1,156 @@
+/// <reference path='../typings/tsd.d.ts' />
+import YAML	= require( 'js-yaml' );
+
+/**
+ * Description of a page or a post
+ */
+class Content
+{
+	
+	/************************************************************/
+	
+	/**
+	 * The front matter for the content
+	 */
+	public frontMatter:FrontMatter = new FrontMatter();
+	
+	/**
+	 * The main content for the post/page
+	 */
+	public mainContent:string = null;
+	
+	/**
+	 * The date of the post, as a Date object
+	 */
+	public date:Date = null;
+	
+	/**
+	 * The name of the post, taken from the filename
+	 */
+	public name:string = null;
+	
+	/**
+	 * The URL for this post
+	 */
+	public url:string = null;
+	
+	/************************************************************/
+	
+	/**
+	 * Reads our content from file
+	 */
+	public readFromFile( filename:string, file:string ):void
+	{		 
+		// get the file contents
+		var a:string[] = file.match( /(^---(?:\r\n|\n))([\s\S]+)((?:\r\n|\n)---(?:\r\n|\n))([\s\S]+)/ );
+		if( a != null )
+		{
+			this.frontMatter.fromObj( YAML.load( a[2] ) );
+			this.mainContent = a[4].trim();
+		}
+		else
+			this.mainContent = file.trim();
+			
+		// set our date
+		if( this.frontMatter.date != null )
+			this.date = new Date( this.frontMatter.date );
+			
+		// create our filename object (get the date and name)
+		a = filename.match( /^(\d{4})-(\d\d?)-(\d\d?)-(.+)(\.html)/ );
+		if( a != null )
+		{
+			this.name = a[4];
+			if( this.date == null )
+				this.date = new Date( Number( a[1] ), Number( a[2] ) - 1, Number( a[3] ) );
+		}
+		else
+			this.name = filename.substr( 0, -5 );
+			
+		// set our url
+		if( this.frontMatter.permalink != null )
+		{
+			if( this.frontMatter.permalink.lastIndexOf( "/" ) != this.frontMatter.permalink.length - 1 )
+				this.frontMatter.permalink += "/";
+			this.url = this.frontMatter.permalink + "index.html";
+		}
+		else
+		{
+			var year:number 	= this.date.getFullYear();
+			var month:number 	= this.date.getMonth() + 1;
+			var day:number		= this.date.getUTCDate();
+			var yearStr:string	= year + "/";
+			var monthStr:string	= ( month < 10 ) ? "0" + month + "/" : month + "/";
+			var dayStr:string	= ( day < 10 ) ? "0" + day + "/" : day + "/";
+			this.url 			= yearStr + monthStr + dayStr + this.name + "/index.html";
+		}
+	}
+	
+}
+
+/**
+ * Describes the front matter for a page/post
+ */
+class FrontMatter
+{
+	
+	/************************************************************/
+	
+	/**
+	 * The layout to use for this page/post
+	 */
+	public layout:string = null;
+	
+	/**
+	 * The permalink to use (default is /year/month/day/title.html)
+	 */
+	public permalink:string = null;
+	
+	/**
+	 * Is this post/page published?
+	 */
+	public published:boolean = true;
+	
+	/**
+	 * The category to set this page/post to
+	 */
+	public category:string = null;
+	
+	/**
+	 * If we want to assign multiple categories to the page/post
+	 */
+	public categories:string[] = []];
+	
+	/**
+	 * The tags to associate with this page/post
+	 */
+	public tags:string[] = []];
+	
+	/**
+	 * The date that will override the date from the name of the post
+	 */
+	public date:string = null;
+	
+	/**
+	 * Does this page/post have any comments? (found in _includes)
+	 */
+	public hasComments:boolean = false;
+	
+	/**
+	 * Does this page/post have any associated files? (found in _includess)
+	 */
+	public hasFiles:boolean = false;
+	
+	/************************************************************/
+	
+	/**
+	 * Fills our data from a generic Object
+	 */
+	public fromObj( obj:Object ):void
+	{
+		for( var key in obj )
+			this[key] = obj[key];
+	}
+	
+}
+
+export = Content;
