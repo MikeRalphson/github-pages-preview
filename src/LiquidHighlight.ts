830d43f5f65c2f0126682e67f754d0ffb2afb843
Damian Connolly <Damian Connolly>
2016-01-02 02:03:49 +0100

    Removed LiquidEndHighlight as it turns out we don't need it - set up a good flow for LiquidHighlight to get ready to parse the code

--- src/LiquidHighlight.ts
+++ src/LiquidHighlight.ts
@@ -4,20 +4,44 @@ import RSVP		= require( 'es6-promise' );
 import Liquid 	= require( 'liquid-node' );
 var Promise		= RSVP.Promise;
 
-class LiquidHighlight extends Liquid.Tag
+class LiquidHighlight extends Liquid.Block
 {
+	
+	/************************************************************/
+
 	public static log:Bunyan.Logger = null;
 	
-	public parse()
+	/************************************************************/
+	
+	private _lang = null; // the language that we're highlighting
+	
+	/************************************************************/
+	
+	/**
+	 * Creates a new LiquidHighlight tag
+	 * @param template The template for our content
+	 * @param tagname The name of this tag (highlight)
+	 * @param markup Any additional markup included with the tag (in this case, the lang that we're highlighting)
+	 */
+	constructor( template:any, tagname:string, markup:string )
 	{
-		LiquidHighlight.log.info( "PARSE!", arguments );
-		return Promise.resolve<void>();
+		super( template, tagname, markup );
+		this._lang = ( markup != null ) ? markup.trim() : null;
 	}
 	
+	/**
+	 * Called when we want to render the contents of our tag
+	 */
 	public render()
 	{
-		LiquidHighlight.log.info( "RENDER", arguments );
-		return "";
+		var lh:LiquidHighlight = this;
+		
+		// get the content as a string, then highlight it
+		// TODO: call highlight.js on this
+		return super.render().then( function( ar:string[] ){
+			var str:string = Liquid.Helpers.toFlatString( ar );
+			return lh._lang + ": " + str.toUpperCase();
+		});
 	}
 }
 
