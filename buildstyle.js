08516014ea19f17f673324c403b8ca8d40da8c41
Mike Ralphson <mike.ralphson@gmail.com>
2018-03-06 09:05:58 +0000

    Prepare for theme support

---
+++ buildstyle.js
@@ -0,0 +1,28 @@
+#!/bin/env node
+
+'use strict';
+
+var fs = require('fs');
+var sass = require('node-sass');
+var assetFunctions = require('node-sass-asset-functions');
+
+var outputStyle = process.argv.length > 2 ? process.argv[2] : 'nested';
+
+function sassRender(infile,outfile) {
+	sass.render({
+		file: infile,
+		outputStyle : outputStyle,
+		functions: assetFunctions({
+			http_fonts_path: '../../source/fonts'
+		})
+	}, function(err, result) {
+		if (err) console.error(err)
+		else {
+			fs.writeFile(outfile,result.css.toString(),'utf8',function(err){
+                if (err) console.warn(err.message);
+            });
+        }
+	});
+}
+
+sassRender('./themes/cayman/_sass/jekyll-theme-cayman.scss','./themes/cayman.css');
